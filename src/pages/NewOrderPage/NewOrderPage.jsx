0f2b0f4738f1da478fb3fe50c96d9176af6d429e
lauren <laurencrosariol@yahoo.com>
2022-06-15 00:13:37 -0700

    initial commit

---
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -0,0 +1,12 @@
+export default function NewOrderPage({categories}) {
+    return (
+        <>
+            <h1>NewOrderPage</h1>
+                <div >
+                {/* {categories.map((category) => (
+                    <div>{category.name}</div>
+                ))} */}
+                </div>
+        </>
+    );
+}
